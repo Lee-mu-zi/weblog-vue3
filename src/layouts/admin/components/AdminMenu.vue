b04f40519598f24b041d06eb8cb3aa6d741a0c68
lee <li15237845367@163.com>
2024-03-31 00:05:39 +0800

    Update

--- src/layouts/admin/components/AdminMenu.vue
+++ src/layouts/admin/components/AdminMenu.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="bg-slate-800 h-screen text-white">
+    <div class="bg-slate-800 h-screen text-white menu-container" :style="{ width: menuStore.menuWidth }">
         <!-- 顶部 Logo, 指定高度为 64px, 和右边的 Header 头保持一样高 -->
         <div class="flex items-center justify-center h-[64px]">
-            <img src="@/assets/weblog-logo.png" class="h-[60px]">
+            <img src="@/assets/weblog-logo-new.svg" class="h-[60px]">
+
+            <!-- 通过下面的语句更换折叠或者展开时的管理界面的logo 暂时无用 注释 -->
+            <!-- <img src=" @/assets/weblog-logo-new.svg" class="h-[60px]" v-if="menuStore.menuWidth == '250px'"> -->
+            <!-- <img src="@/assets/logo.svg" class="h-[60px]" v-else> -->
         </div>
 
         <!-- 下方菜单 -->
-        <el-menu default-active="defaultActive" @select="handleSelect">
+        <el-menu :default-active="defaultActive" @select="handleSelect" :collapse="isCollapse"
+            :collapse-transition="false">
             <template v-for="(item, index) in menus" :key="index">
                 <el-menu-item :index="item.path">
                     <el-icon>
@@ -53,9 +58,14 @@
 </style>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+import { useMenuStore } from '@/stores/menu'
 
+// 引入 useMenuStore
+const menuStore = useMenuStore()
+//是否折叠
+const isCollapse = computed(() => !(menuStore.menuWidth == '250px'))
 const route = useRoute()
 const router = useRouter()
 
