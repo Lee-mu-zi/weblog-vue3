6315371056e2652fe41fec78b0caa6d5b451f120
lee <li15237845367@163.com>
2024-06-13 18:54:57 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ViteCompressionPlugin from 'vite-plugin-compression'
+import { visualizer } from 'rollup-plugin-visualizer'
+import importToCDN from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,6 +29,22 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    ViteCompressionPlugin({
+      filter: /\.(js|css)$/i, // 只压缩 js 和 css 文件
+      threshold: 1024, // 只压缩大于1KB的文件
+      algorithm: 'gzip', // 使用 gzip 算法
+      ext: '.gz', // 压缩文件的扩展名
+    }),
+    importToCDN({
+      modules: [
+        {
+          name: 'echarts',
+          var: 'echarts',
+          path: 'https://cdn.bootcdn.net/ajax/libs/echarts/5.4.2/echarts.min.js',
+        },
+      ],
+    }),
+    visualizer()
   ],
   resolve: {
     alias: {
